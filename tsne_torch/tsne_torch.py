18e4b5f27cc260bcb2294142bfb340df3a5c49f4
Palle Klewitz <palleklewitz@icloud.com>
2021-05-08 04:23:53 +0200

    notebook detection

--- tsne_torch/tsne_torch.py
+++ tsne_torch/tsne_torch.py
@@ -17,7 +17,26 @@ from typing import Union
 
 import numpy as np
 import torch
-from tqdm import tqdm
+
+
+def is_notebook():
+    try:
+        # noinspection PyUnresolvedReferences
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
+
+
+if is_notebook():
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
 
 
 # noinspection PyPep8Naming
