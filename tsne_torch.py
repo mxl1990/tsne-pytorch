cd95bfe8ddbce2609164b5adeeae3be28f3cc33a
mxl1990 <mxl1990@gmail.com>
2022-01-04 15:53:52 +0800

    Update tsne_torch.py
    fix bug for range in pca_torch function

--- tsne_torch.py
+++ tsne_torch.py
@@ -116,9 +116,14 @@ def pca_torch(X, no_dims=50):
 
     (l, M) = torch.eig(torch.mm(X.t(), X), True)
     # split M real
-    for i in range(d):
+    # this part may be some difference for complex eigenvalue
+    # but complex eignevalue is meanless here, so they are replaced by their real part
+    i = 0
+    while i < d:
         if l[i, 1] != 0:
             M[:, i+1] = M[:, i]
+            i += 2
+        else:
             i += 1
 
     Y = torch.mm(X, M[:, 0:no_dims])
@@ -230,3 +235,7 @@ if __name__ == "__main__":
 
     pyplot.scatter(Y[:, 0], Y[:, 1], 20, labels)
     pyplot.show()
+
+    # X1 = torch.randn([10, 2048]).cuda()
+    # # no_dims=2, initial_dims=50, perplexity=30.0
+    # X_emb = tsne(X1, no_dims=2, perplexity=100., initial_dims=50)
